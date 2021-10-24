be02ab66d7220cfcd4576dfb4f0e3e109710ec11
Ivanildo Ferreira <isivanildo@hotmail.com>
2021-10-24 13:04:13 -0300

    Trabalahndo o evento do botão submit do formulário

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,22 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
-fields.forEach(function(field, index) {
+document.getElementById("form-user-create").addEventListener("submit", function(event){
+    
+    event.preventDefault();
 
-    if (field.name == "gender") {
-        if (field.checked) {
+    fields.forEach(function(field, index) {
+
+        if (field.name == "gender") {
+            if (field.checked) {
+                user[field.name] = field.value;
+            }
+        } else {
             user[field.name] = field.value;
         }
-    } else {
-        user[field.name] = field.value;
-    }
+    
+    })
+
+    console.log(user);
 
 })
-
-console.log(user);
