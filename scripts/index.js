5bcf03d6a8ba27abbb84d4f6c887c9b93231f9bd
Ivanildo Ferreira <isivanildo@hotmail.com>
2021-10-24 16:44:57 -0300

    Criando a classe User.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,22 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
+function addLine(userData) {
+
+    document.getElementById("table-user").innerHTML = `
+        <tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${userData.name}</td>
+            <td>${userData.email}</td>
+            <td>${userData.admin}</td>
+            <td>${userData.birth}</td>
+            <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>`
+}
+
 document.getElementById("form-user-create").addEventListener("submit", function(event){
     
     event.preventDefault();
@@ -17,6 +33,18 @@ document.getElementById("form-user-create").addEventListener("submit", function(
     
     })
 
-    console.log(user);
+    var objetoUser = new User(
+        user.name, 
+        user.gender, 
+        user.birth, 
+        user.country,
+        user.email, 
+        user.password, 
+        user.phoyo, 
+        user.admin
+        );
+
+
+    addLine(objetoUser);
 
 })
